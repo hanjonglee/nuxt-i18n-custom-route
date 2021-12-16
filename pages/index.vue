260c296b5bdb5d7a3fd05fa5156ce5b0938078bd
ivan <121there@gmail.com>
2021-12-16 23:56:46 +0900

    feat: add i18n usage example

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <div>
+    <nuxt-link
+      v-for="locale in i18n.locales"
+      :key="locale.code"
+      :to="{ query: { hl: locale.code } }"
+      tag="button"
+    >
+      {{ locale.name }}
+    </nuxt-link>
+    <h1>{{ $t("test") }}</h1>
+  </div>
 </template>
 
 <script>
+import i18n from "@/i18n.config";
+
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return { i18n };
+  },
+};
 </script>
